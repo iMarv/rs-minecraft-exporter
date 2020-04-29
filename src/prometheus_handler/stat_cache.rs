d835e0a1a2499489c933d440589a41baea5444d0
Marvin Altemeier <marv.internetz@gmail.com>
2020-04-29 23:51:54 +0200

    Only use single hashmap for caching stats

--- src/prometheus_handler/stat_cache.rs
+++ src/prometheus_handler/stat_cache.rs
@@ -3,20 +3,21 @@ use prometheus::{default_registry, Counter, Registry};
 use std::{collections::HashMap, sync::Arc};
 use tokio::sync::Mutex;
 
-type Uuid = String;
-type CategoryTypeCache = HashMap<String, Counter>;
-type CategoryCache = HashMap<StatCategory, CategoryTypeCache>;
-type PlayerCache = HashMap<Uuid, CategoryCache>;
+type CounterCache = HashMap<String, Counter>;
+
+lazy_static! {
+    static ref STAT_CACHE: StatCache = StatCache::new();
+}
 
 struct StatCache {
-    player_cache: Arc<Mutex<PlayerCache>>,
+    counter_cache: Arc<Mutex<CounterCache>>,
     registry: &'static Registry,
 }
 
 impl Default for StatCache {
     fn default() -> Self {
         Self {
-            player_cache: Arc::new(Mutex::new(HashMap::new())),
+            counter_cache: Arc::new(Mutex::new(HashMap::new())),
             registry: default_registry(),
         }
     }
@@ -38,40 +39,43 @@ impl StatCache {
         counter.inc_by(value - counter.get());
     }
 
-    async fn get_category_cache(&self, player: &Player) -> CategoryCache {
-        let mut player_cache = self.player_cache.lock().await;
-
-        if !player_cache.contains_key(&player.uuid) {
-            player_cache.insert(player.uuid.clone(), HashMap::new());
-        }
-
-        player_cache.get(&player.uuid).unwrap().clone()
-    }
-
-    async fn get_type_cache(&self, player: &Player, category: &StatCategory) -> CategoryTypeCache {
-        let mut category_cache = self.get_category_cache(player).await;
-
-        if !category_cache.contains_key(category) {
-            category_cache.insert(category.clone(), HashMap::new());
-        }
-
-        category_cache.get(category).unwrap().clone()
-    }
-
     pub async fn get_counter(
         &self,
         player: &Player,
         category: &StatCategory,
         category_type: &String,
     ) -> Counter {
-        let category_type_cache = self.get_type_cache(player, category).await;
+        let id = unique_stat_id(player, category, category_type);
+        let mut counter_cache = self.counter_cache.lock().await;
 
-        if !category_type_cache.contains_key(category_type) {
-            todo!();
+        if !counter_cache.contains_key(&id) {
+            let labels: HashMap<&str, &String> = labels!(
+                "player" => &player.name,
+                "type" => category_type,
+            );
+
+            let (category_name, category_help) = get_category_metadata(category);
+
+            let counter = Counter::with_opts(opts!(
+                &String::from(&category_name),
+                &String::from(&category_help),
+                labels
+            ))
+            .unwrap();
+
+            self.registry
+                .register(Box::new(counter.clone()))
+                .expect("Stat got registered twice. This should not happen.");
+
+            counter_cache.insert(id.clone(), counter);
         }
 
-        category_type_cache.get(category_type).unwrap().clone()
+        counter_cache.get(&id).unwrap().clone()
     }
+}
+
+fn unique_stat_id(player: &Player, category: &StatCategory, category_type: &String) -> String {
+    format!("{}_{}_{}", &player.uuid, category, category_type)
 }
 
 fn get_category_metadata(category: &StatCategory) -> (String, String) {
@@ -93,36 +97,37 @@ mod tests {
     use prometheus::Registry;
 
     macro_rules! setup_cache {
-        ($player_id:expr, $stat_category:expr, $category_type:expr, $value:expr) => {{
-            let category_type_cache = {
+        ($player:expr, $stat_category:expr, $category_type:expr, $value:expr) => {{
+            let counter_cache = {
+                let id = unique_stat_id($player, $stat_category, $category_type);
                 let counter = Counter::with_opts(opts!("Testi", "Help")).unwrap();
                 counter.inc_by($value);
 
-                let mut c: CategoryTypeCache = HashMap::new();
-                c.insert(String::from($category_type), counter);
+                let mut c = HashMap::new();
+                c.insert(id, counter);
 
-                c
-            };
-
-            let category_cache: CategoryCache = {
-                let mut c: CategoryCache = HashMap::new();
-                c.insert($stat_category, category_type_cache);
-
-                c
-            };
-
-            let player_cache: PlayerCache = {
-                let player = mock_player!($player_id);
-                let mut c: PlayerCache = HashMap::new();
-                c.insert(player.uuid, category_cache);
                 c
             };
 
             StatCache {
-                player_cache: Arc::new(Mutex::new(player_cache)),
+                counter_cache: Arc::new(Mutex::new(counter_cache)),
                 registry: default_registry(),
             }
         }};
+    }
+
+    mod get_category_metadata {
+        use super::*;
+
+        #[test]
+        fn should_remove_prefix() {
+            let category = StatCategory::Broken;
+
+            let (actual, _) = get_category_metadata(&category);
+            let expected = String::from("mc_broken");
+
+            assert_eq!(actual, expected);
+        }
     }
 
     mod set_stat {
@@ -132,10 +137,10 @@ mod tests {
         async fn should_update_existing_stat() {
             let player = &mock_player!(1);
             let category = &StatCategory::Crafted;
-            let category_type = &String::from("minecraft:testo");
+            let category_type = &String::from("minecraft:test1");
             let value = 2.0;
 
-            let cache = setup_cache!(1, StatCategory::Crafted, category_type, value);
+            let cache = setup_cache!(&player, category, category_type, value);
 
             let value = 5.0;
             cache.set_stat(player, category, category_type, value).await;
@@ -148,14 +153,14 @@ mod tests {
         #[tokio::test]
         async fn should_insert_new_stat() {
             let cache = setup_cache!(
-                3,
-                StatCategory::Broken,
+                &mock_player!(3),
+                &StatCategory::Broken,
                 &String::from("minecraft:testi"),
                 67.0
             );
             let player = &mock_player!(1);
             let category = &StatCategory::Crafted;
-            let category_type = &String::from("minecraft:testo");
+            let category_type = &String::from("minecraft:test2");
             let value = 2.0;
 
             cache.set_stat(player, category, category_type, value).await;
